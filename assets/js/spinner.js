a2bcc3532cbde34f77fedade3dd742c5aef4c247
tornikeo <tornikeonoprishvili@gmail.com>
2022-11-02 23:42:27 +0400

    Add another spinner style

--- assets/js/spinner.js
+++ assets/js/spinner.js
@@ -1,9 +1,7 @@
 $(document).ready(function() {
     const [spinner, iframe] = $(".iframe-with-spinner").children();
-    console.log( spinner, iframe )
     $(spinner).show()
     $(iframe).on("load", (function() {
-        console.log("Load ended");
         $(spinner).hide()
         $(iframe).removeAttr('hidden');
     }))
