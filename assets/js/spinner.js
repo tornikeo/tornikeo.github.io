c11ba78727329b95422f027dea9e705e834044bd
tornikeo <tornikeonoprishvili@gmail.com>
2022-11-14 19:38:02 +0400

    Set hard limit on spinner swap 20 secs

--- assets/js/spinner.js
+++ assets/js/spinner.js
@@ -1,7 +1,23 @@
 $(document).ready(function() {
+    // let intervalId;
+    // intervalId = setInterval(() => {
+    //     console.log('Checking...');
+    //     console.log(document.querySelector('#iframe'));
+    //     if ($("gradio-app").length > 0) {
+    //         console.log('YES');
+    //         $("#spinner").hide()
+    //         $("#iframe").removeAttr('hidden')
+    //         clearInterval(intervalId);
+    //     } else {
+    //         console.log('Nope');
+    //     }
+    // }, 500)
+    
     $("#spinner").show()
-    $("#iframe").ready(function() {
+    function spinner_swap() {
         $("#spinner").hide()
         $("#iframe").removeAttr('hidden')
-    })
+    }
+    $("#iframe").ready(spinner_swap)
+    setTimeout(spinner_swap, 20000)
 })
